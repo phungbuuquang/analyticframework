5d1bf8e7f15e2d62cba5524a2c137a651451ceba
quangphung <alexalexwan1@gmail.com>
2021-05-12 20:41:46 +0700

    add

---
+++ AnalyticFramework/AnalyticFramework.h
@@ -0,0 +1,18 @@
+//
+//  AnalyticFramework.h
+//  AnalyticFramework
+//
+//  Created by quangphung on 5/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnalyticFramework.
+FOUNDATION_EXPORT double AnalyticFrameworkVersionNumber;
+
+//! Project version string for AnalyticFramework.
+FOUNDATION_EXPORT const unsigned char AnalyticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticFramework/PublicHeader.h>
+
+
---
+++ AnalyticFramework/Analytics.swift
@@ -0,0 +1,15 @@
+//
+//  Analytics.swift
+//  AnalyticFramework
+//
+//  Created by quangphung on 5/12/21.
+//
+
+import Foundation
+
+public class Analytics{
+    public init(){}
+    public func log(message: String){
+        print("Log messae: ", message)
+    }
+}
